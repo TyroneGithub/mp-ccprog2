ac4b7a0f0c62264f4cd21023fa576c78f1e351d4
Tyrone Justin  R. Sta. Maria <11945451@dlsu.edu.ph>
2020-02-18 10:03:19 +0800

    added test values

--- filesTest.c
+++ filesTest.c
@@ -46,10 +46,61 @@ typedef struct Transactions{
 
 
 int main(){
-	struct Users aUser[MAX_USERS];
+	struct Users aUser[MAX_USERS] = {
+		{
+			1, "Tyrone", "123456", "49 Macopa st.", 9153001
+		},
+		{
+			2, "Justin", "666667777", "10 Don manuel", 12345
+		}
+	};
 	struct Items aItem[MAX_USERS][MAX_ITEMS];
 	struct Transactions aTransaction[MAX_USERS];
+	int numUsers = aUser[1].userId;
+	int option; 
+	int i;
+	FILE *fp_user;
 	
+	fp_user = fopen("Users.txt", "w");
+	
+	
+	printf("Shopping App\n");
+	
+	for(i = 0; i < numUsers; i++){
+		fprintf(fp_user, "%d %s\n", aUser[i].userId, aUser[i].password);
+		fprintf(fp_user, "%s\n%s",aUser[i].name, aUser[i].address);
+		fprintf(fp_user, "%d\n\n",aUser[i].contactNum);
+	}
+	
+	
+//	do{
+//		printf("[1] Register\n[2] Login\n[3] Exit\n");
+//		printf("Input: ");
+//		scanf("%d", &option);
+//		fflush(stdin);
+//		switch(option){
+//			case 1:
+//				registerUser(aUser, aItem, aTransaction, &numUsers);
+//				break;
+//			case 2:
+//				printf("login\n");
+//				break;
+//			case 3:
+//				for(i = 0; i < numUsers; i++){
+//					fprintf(fp_user, "%d %s\n", aUser[i].userId, aUser[i].password);
+//					fprintf(fp_user, "%s\n%s",aUser[i].name, aUser[i].address);
+//					fprintf(fp_user, "%d\n\n",aUser[i].contactNum);
+//				}
+//				
+//				break;
+//			default:
+//				break;
+//		}
+//		
+//	}while(option >= 1 && option <= 2);
+	 
+	fclose(fp_user);
+	return 0;
 	
 	return 0;
 }
